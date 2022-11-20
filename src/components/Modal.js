bc1bb481045a29bcffb46e7cd33e840cb88baa1e
Madeline Yi <sunhunfunbun@gmail.com>
2022-11-20 15:00:51 -0600

    updated the css for several components

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,11 @@ import './Modal.css'
 export default function Modal({children, handleClose}) {
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
-        <div className="modal">
+        <div className="modal" style={{
+          border: "4px solid", 
+          borderColor: "#09b3d9",
+          textAlign: "center"
+          }}>
             {children}
             <button onClick={handleClose}>close</button>
         </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,8 @@
+import './Title.css';
+
 export default function Title({title, subtitle}) {
     return (
-        <div>
+        <div classNAme ="title-block">
             <h1 className="title">{title}</h1>
             <br />
             <h2 className="subtitle">{subtitle}</h2>
